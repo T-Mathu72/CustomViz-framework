4b6d2a4580af7774ef8c7774aff46119ec681722
Mathurin Theo <tmathurin@datatorii.com>
2026-04-17 10:34:19 +0200

    Modification du Mot de passe

--- config.js
+++ config.js
@@ -12,7 +12,7 @@
 const SUPABASE_URL  = 'https://fcgdefombxdwpeqmqsaa.supabase.co';   // ex: https://xxxx.supabase.co
 const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjZ2RlZm9tYnhkd3BlcW1xc2FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNjM5MzAsImV4cCI6MjA5MTgzOTkzMH0.GTl7hmYRH0wh7f5Hwsn5zjNOZ6Mn8ZHlLLY8hxd7Gfw';        // clé publique anon
 
-const ADMIN_PASSWORD = 'datatorii2024';   // ← changez ce mot de passe
+const ADMIN_PASSWORD = '69 la team';   // ← changez ce mot de passe
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
